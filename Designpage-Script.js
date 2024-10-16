a5c386c9ffa856bfce4cd8f67a9ee15e11ed89be
PrinceEnas <157136093+PrinceEnas@users.noreply.github.com>
2024-10-16 02:04:55 +0100

    Push committed

---
+++ Designpage-Script.js
@@ -0,0 +1,67 @@
+let count = 0;
+
+document.getElementById("subtract").onclick = function(){
+    count -=  1;
+    document.getElementById("counter").innerHTML = count;
+
+    document.getElementById("itemSelected").innerHTML = "125.00 x " + count + " "; 
+
+    document.getElementById("counter").innerHTML = count;
+
+    document.getElementById("itemTotal").innerHTML = count * 125 + ".00";
+
+    if(count < 0){
+       return document.getElementById("counter").innerHTML = (count = 0);
+
+    }
+    
+}
+
+document.getElementById("add").onclick = function(){
+    count +=  1;
+    document.getElementById("counter").innerHTML = count;
+
+    document.getElementById("itemSelected").innerHTML = "125.00 x " + count + " "; 
+
+    document.getElementById("counter").innerHTML = count;
+
+    document.getElementById("itemTotal").innerHTML = count * 125  + ".00";
+
+
+    if(count < 0){
+        return document.getElementById("counter").innerHTML = (count = 0);
+    }
+}
+
+
+
+function displayImg1Change(){
+    document.getElementById("DisplayImg").src = "images/image-product-1.jpg";
+}
+
+function displayImg2Change(){
+    document.getElementById("DisplayImg").src = "images/image-product-2.jpg";
+}
+
+function displayImg3Change(){
+    document.getElementById("DisplayImg").src = "images/image-product-3.jpg";
+}
+
+function displayImg4Change(){
+    document.getElementById("DisplayImg").src = "images/image-product-4.jpg";
+}
+
+function dropboxDisplay(){
+  document.getElementById("showdisplay").style.display= "block";
+
+  setTimeout(function(){
+    document.getElementById("showdisplay").style.display= "none";
+},3000);
+}
+
+function ClearSelecteditem(){
+    document.getElementById("dropIteminfo").innerHTML = "Box is Empty";
+}
+
+
+
